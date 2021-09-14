3ce2dfecc53c4f2cd21e6e2a2fbc764f78f5f43b
brocklarson <brock.larson0725@gmail.com>
2021-09-14 08:49:28 -0600

    Add cards for books basic functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-const addBook = document.getElementById('submitButton');
+const submitButton = document.getElementById(`submitButton`);
+const addBookButton = document.getElementById(`addBookButton`);
 let myLibrary = [{
-    title: 'The Hobbit',
-    Author: 'J.R.R. Tolkien',
-    Pages: 610,
+    title: `The Hobbit`,
+    author: `J.R.R. Tolkien`,
+    pages: 610,
     isRead: true,
-    comments: ''
+    comments: `Love it! Need to read again soon. The character development in this book is the best that I have ever seen`
 }];
-
-addBook.addEventListener('click', addBookToLibrary);
 
 function Book(title, author, pages, isRead, comments) {
     this.title = title
@@ -17,17 +16,77 @@ function Book(title, author, pages, isRead, comments) {
     this.comments = comments
 }
 
-function addBookToLibrary(event) {
+createCard();
+submitButton.addEventListener(`click`, operate);
+addBookButton.addEventListener(`click`, showForm);
+
+
+function operate(event) {
     event.preventDefault();
+    addBookToLibrary();
+    hideForm();
+    createCard();
+
+}
+
+function addBookToLibrary(event) {
     const newBook = createBook();
     myLibrary.push(newBook);
 }
 
 function createBook() {
-    const title = addBookForm.elements['bookTitle'].value;
-    const author = addBookForm.elements['bookAuthor'].value;
-    const pages = addBookForm.elements['pageCount'].value;
-    const isRead = addBookForm.elements['isReadSwitch'].checked;
-    const comments = addBookForm.elements['optionalComments'].value;
+    const title = addBookForm.elements[`bookTitle`].value;
+    const author = addBookForm.elements[`bookAuthor`].value;
+    const pages = parseInt(addBookForm.elements[`pageCount`].value);
+    const isRead = addBookForm.elements[`isReadSwitch`].checked;
+    const comments = addBookForm.elements[`optionalComments`].value;
     return new Book(title, author, pages, isRead, comments);
+}
+
+function createCard() {
+    if (!myLibrary.length) return;
+
+    const currentBook = myLibrary.at(-1);
+    console.table(currentBook);
+
+    const bookCard = document.createElement(`div`);
+    const bookCardHeader = document.createElement(`div`);
+    const editBook = document.createElement(`span`);
+    const removeBook = document.createElement(`span`);
+    const bookTitle = document.createElement(`h3`);
+    const bookAuthor = document.createElement(`h4`);
+    const bookPages = document.createElement(`h4`);
+    const bookComments = document.createElement(`p`);
+
+    bookCard.classList.add(`book-card`);
+    bookCardHeader.classList.add(`book-card-header`);
+    editBook.classList.add(`material-icons-outlined`, `edit`);
+    removeBook.classList.add(`material-icons-outlined`, `remove`);
+
+    bookTitle.innerText = currentBook.title;
+    bookAuthor.innerText = `By: ${currentBook.author}`;
+    bookPages.innerText = `Pages: ${currentBook.pages}`;
+    bookComments.innerText = currentBook.comments;
+    editBook.innerText = `edit`;
+    removeBook.innerText = `close`;
+
+    document.getElementById(`bookCardContainer`).appendChild(bookCard);
+    bookCard.appendChild(bookCardHeader);
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookComments);
+    bookCardHeader.appendChild(editBook);
+    bookCardHeader.appendChild(removeBook);
+
+}
+
+function showForm() {
+    const formContainer = document.getElementById(`formContainer`);
+    formContainer.classList.add(`show`);
+}
+
+function hideForm() {
+    const formContainer = document.getElementById(`formContainer`);
+    formContainer.classList.remove(`show`);
 }
